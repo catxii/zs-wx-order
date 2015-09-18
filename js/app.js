88a7bfdfd228ff8c56ef3c4b9e63d2122955dc2c
catxii <catxii@qq.com>
2015-09-18 09:33:39 +0800

    提交酒店预订页面

--- js/app.js
+++ js/app.js
@@ -7,3 +7,23 @@ function showModal(id){
 $(".close-btn").click(function(){
   $(this).closest(".mask").removeClass("in");
 })
+
+// 订单成功提交
+
+ $(document).on('click','#confirm-title-ok-cancel', function () {
+ 	
+ 	$.modal({
+      text: '<div class="success-dialog"><h1>您的订单已成功提交!</h1><h6>我们将尽快处理，请耐心等待。</h6></h1></div>',
+      buttons: [
+        {
+    	  text: '<span class="unimportance-color">继续逛逛</span>'
+        },
+        {
+          text: '<span class="warning-color">查看订单</span>',
+          onClick: function() {
+            $.alert('你点击了查看订单！')
+          }
+        },
+      ]
+    })
+  });
